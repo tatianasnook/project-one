f592b2971d576697bc0b3051e35634e2f63d5a68
tatianasnook <tatsnook@gmail.com>
2024-07-05 14:45:21 -0500

    completed styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,11 @@ import icon from './react-icon.png';
 const Navbar = () => {
   return (
     <nav className="nav">
-      <img src={ icon } alt="React icon" className='nav-icon' />
-      <h3 className='nav-logo-text'>ReactFacts</h3>
-      <h4 className='nav-title'>React Course - Project 1</h4>
+      <div className='nav-container'>
+        <img src={ icon } alt="React icon" className='nav-icon' />
+        <h3 className='nav-logo-text'>ReactFacts</h3>
+        <h4 className='nav-title'>React Course - Project 1</h4>
+      </div>
     </nav>
   )
 }
